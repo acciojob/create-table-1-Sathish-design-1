5cdd08e7984cd2d8901e21b16844f6f482ac4d7f
SATHISH B <sathishb2310@gmail.com>
2025-11-12 15:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+  const table = document.getElementById("sampleTable");
+
+	const newrow = table.insertRow(0);
+
+	const cell1 = newrow.insertCell(0);
+	const cell2 = newrow.insertCell(2);
+
+	cell1.innerHTML = "New Cell1";
+	cell2.innerHTML = "New Cell2";
   
 }
